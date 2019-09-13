f2fe0a388b2b3f6d320a4fd08a7eb16740069e40
Pedro Muñoz <pmz94@hotmail.com>
2019-09-12 17:27:03 -0700

    .

--- 01-hola-mundo/app3.js
+++ 01-hola-mundo/app3.js
@@ -2,14 +2,12 @@ console.log('Inicio del programa');
 
 setTimeout(function() {
     console.log('1er Timeout');
-}, 3000);
+}, 2000);
 
-setTimeout(function() {
-    console.log('2do Timeout');
-}, 0);
+setTimeout(() => console.log('2do Timeout'), 0);
 
-setTimeout(function() {
-    console.log('3er Timeout');
-}, 0);
+setTimeout(_ => console.log('3er Timeout'), 0);
+
+Promise.resolve().then(_ => console.log('Promesa'));
 
 console.log('Fin del programa');
